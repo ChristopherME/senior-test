785b1e3938a69936b0c0732f82cb6b40fbfc8d0b
Christopher Elias <celias@peruapps.com.pe>
2019-08-28 09:12:48 -0500

    change(BREAKING): make baseViewModel navigator a weak reference for make the garbage collector clear it from memory.

--- app/src/main/java/com/peruapps/christopher_elias/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/peruapps/christopher_elias/ui/base/BaseViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
+import java.lang.ref.WeakReference
 
 /**
  * Created by Christopher Elias on 29-may-2019
@@ -15,7 +16,12 @@ import io.reactivex.disposables.CompositeDisposable
 
 abstract class BaseViewModel <T> : ViewModel() {
 
-    private var navigator: T? = null
+    //TODO: Change this. Use Navigation Components from Android JetPack.
+    //      This is just a temporal solution to fix memory leaks due to the reference to activity.
+    //      The final solution will be using Navigation with multiple fragments and one activity.
+    //      By making navigator weakReference the garbage collector will be able to clean the  "context"
+    //      from memory after the viewmodel has been cleared..
+    private var navigator: WeakReference<T>? = null
 
     // Shows or hide progress loading bar if the have it.
     val isLoading = ObservableField<Boolean>(true)
@@ -31,19 +37,21 @@ abstract class BaseViewModel <T> : ViewModel() {
 
     // Clear compositeDisposable of the instance
     override fun onCleared() {
-        super.onCleared()
+        navigator?.clear()
         if (!compositeDisposable.isDisposed) {
             compositeDisposable.dispose()
         }
+        super.onCleared()
     }
     //endregion
 
+
     fun getNavigator() : T {
-        return navigator!!
+        return navigator?.get()!!
     }
 
     fun setNavigator(navigator: T) {
-        this.navigator = navigator
+        this.navigator = WeakReference(navigator)
     }
 
     fun logE(message: String?){
